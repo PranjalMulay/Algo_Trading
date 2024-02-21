0263f9ba4c760cc8f2e03f857af95cb2a24bc497
KashyapMavani <kmavani1807@gmail.com>
2024-02-21 12:59:07 +0530

    Established WebSocket connection and Data Fetched

---
+++ client.js
@@ -0,0 +1,69 @@
+const WebSocket = require('ws');
+
+let Data;
+let OpenData;
+let CloseData;
+let HighData;
+let LowData;
+const OpenValues = [];
+const CloseValues = [];
+const HighValues = [];
+const LowValues = [];
+
+
+const ws = new WebSocket("ws://localhost:3000/"); // Replace with your server URL and port
+
+ws.onopen = function() {
+    console.log("WebSocket connection opened!");
+    // You can send data to the server now
+    ws.send("Hello from JavaScript!");
+};
+
+
+ws.onmessage = function(event) {
+    let message = event.data;
+    message = JSON.parse(message)
+    Data = message
+};
+
+setTimeout(
+
+    ()=>{
+        const {Open, Close, High, Low} =  Data;
+        
+        for (const key in Open) {
+            const value = Open[key];
+            OpenValues.push(value);
+          }
+        
+        for (const key in Close) {
+            const value = Close[key];
+            CloseValues.push(value);
+          }
+        
+        for (const key in High) {
+            const value = High[key];
+            HighValues.push(value);
+        }
+
+        for (const key in Low) {
+            const value = Low[key];
+            LowValues.push(value);
+          }
+        
+        console.log(OpenValues);
+        console.log(CloseValues);
+        console.log(HighValues);
+        console.log(LowValues);
+    }
+    ,1500)
+
+
+// ws.onerror = function(error) {
+//     console.error("WebSocket error:", error);
+// };
+
+// ws.onclose = function(event) {
+//     const reason = event.reason;
+//     console.log("WebSocket connection closed:", reason);
+// };
---
+++ server.py
@@ -0,0 +1,28 @@
+import asyncio
+import websockets
+import pandas as pd
+import yfinance as yf
+
+#---------------------------------------------------------------
+
+# SYMBOL = "^NSEI" # any symbol from yahoo finance
+# data = yf.download(SYMBOL,period ="3d",interval = "15m")
+# data = data.to_json()
+
+#---------------------------------------------------------------
+
+
+async def handler(websocket):
+    async for message in websocket:
+        print(f"Received message from client: {message}")
+
+        SYMBOL = "^NSEI" # any symbol from yahoo finance
+        data = yf.download(SYMBOL,period ="1d",interval = "5m")
+        data = data.to_json()
+        # print(type(data))
+        
+        await websocket.send(data)
+
+start_server = websockets.serve(handler, "localhost", 3000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
